6977035551f7f1ce03a4c5016c79e272aa9a98fc
stefischer <stefischer@mail.com>
2018-02-03 17:12:20 -0500

    Added computer opponent, sprites for both fighter and character, different sates for each character (attack and idle), animations for attacking, cleaner and commented js code, removing most global variables, fixed a bug where enemy health wouldn't reach zero if enemy health was less than attackers damage output, that same bug now effects enemy vs attacker instead of vice versa, enemy uses random number generator for damage output

--- js/combatFunctions.js
+++ js/combatFunctions.js
@@ -1,89 +1,119 @@
-//======== Effect Health ========
-/*
-function takeDamage(int){
-    let healthDisplay = document.querySelector(`.healthDisplay`);
-    let health = 100;
-    let currentHealth = healthDisplay.style.width;
-    console.log(`health was taken`);
-    health -= int;
-    healthDisplay.style.width = (health + `%`);
-}
-
-function enemyTakeDamage(int){
-    let enemyHealthDisplay = document.querySelector(`.enemyHealthDisplay`);
-    let enemyHealth = 100;
-    console.log(`enemy health was taken`);
-    enemyHealth -= int;
-    enemyHealthDisplay.style.width = (enemyHealth + `%`);
-    return enemyHealthDisplay.style.width = (enemyHealth + `%`);
-}
-*/
-//======== Attack ========
-
+//======== Global Variables ========
 let enemyHealthDisplay = document.querySelector(`.enemyHealthDisplay`);
+let enemyHealth = storeEnemyHealth();
+let playerHealth = storePlayerHealth();
 
-function handleClickEvent(evnt){
-    //takeDamage();
-    
-    let enemyHealth = 100;
-    let int = 0;
-    
-    switch(sum){
-        case: `1`,
-            int = 10;
-        break;
-        case: `2`,
-            int = 20;
-        break;
-        case: `3`,
-            int = 30;
-        break;
-        case: `4`;
-            int = 40;
-        break;
-        default:
-            int = 0;
-        break;
-            
-    }
-
-    enemyHealth -= int;
-    let currentEnemyHealth = enemyHealthDisplay.style.width = (enemyHealth + `%`);
-    if(currentEnemyHealth === `0%`){
-    attackButton();
-    }
+function storePlayerHealth(){
+    let playerHealth = 100;
+    return playerHealth;
 }
 
-function takeDamage(){
-    let enemyHealthDisplay = document.querySelector(`.enemyHealthDisplay`);
+function storeEnemyHealth(){
     let enemyHealth = 100;
-    let int = 0;
-    
-    if(document.querySelector(`.attackOne`)){
-        int = 10;
-    }
-    if(document.querySelector(`.attackTwo`)){
-        int = 20;
-    }
-    if(document.querySelector(`.attackThree`)){
-        int = 30;
-    }
-    if(document.querySelector(`.attackFour`)){
-        int = 40;
-    }
-
-    enemyHealth -= int;
-    enemyHealthDisplay.style.width = (enemyHealth + `%`);
+    return enemyHealth;
 }
+/*======== End ========*/
 
-function attackButton(){
+//======== Button Functionality ========
+function attackButton(){                                                    //Give Attack Buttons Click Events
     let attack = document.querySelectorAll(`.attack`);
     for(let i = 0; i < attack.length; i++){
         temp = attack[i];
         temp.addEventListener(`click`, handleClickEvent);
     }
 }
+
+function handleClickEvent(evnt){                                            //What happens when button is clicked
+    let value = evnt.target.dataset.increment;
+    
+    if(enemyHealth < value){
+        value = enemyHealth;
+    }
+    takeDamage(value);
+    if(enemyHealth <= 0){
+        winFight();
+    }
+
+    attackAnimKnightOne();
+    setTimeout(enemyAttack, 1000);
+    setTimeout(attackAnimKnightTwo, 1000);
+}
+/*======== End ========*/
+
+//======== Damage Functions ========
+function takeDamage(int){                                                   //Player Deals Damage
+    let enemyHealthDisplay = document.querySelector(`.enemyHealthDisplay`);
+
+    enemyHealth -= int;
+    enemyHealthDisplay.style.width = (enemyHealth + `%`);
+}
+
+function enemyAttack(){                                                     //Enemy Deals Damage
+    let playerHealthDisplay = document.querySelector(`.healthDisplay`);
+    //let enemyInt = Math.floor(Math.random() * 30);
+    let enemyInt = testGen();
+    
+    
+    if(playerHealth < enemyInt){
+        enemyInt = enemyHealth;
+    }
+    
+    playerHealth -= enemyInt;
+    playerHealthDisplay.style.width = (playerHealth + `%`);
+}
+/*======== End ========*/
+
+//======== Knight One States ========
+
+function attackAnimKnightOne(){                                              //Knight One Set Attack
+    let idle = document.querySelector(`.knightIdle`);
+    document.querySelector(`.spriteContainer`).removeChild(idle);
+    $(`.spriteContainer`).append(`<div class="knightAttack"></div>`);
+    
+    
+    setTimeout(setIdleKnightOne, 1000);
+}
+
+function setIdleKnightOne(){                                                //Knight One Set Idle
+    let attack = document.querySelector(`.knightAttack`);
+    document.querySelector(`.spriteContainer`).removeChild(attack);
+    
+    $(`.spriteContainer`).append(`<div class="knightIdle"></div>`)
+}
+/*======== End ========*/
+
+//======== Knight Two States ========
+
+function attackAnimKnightTwo(){                                              //Knight Two Set Attack
+    let idle = document.querySelector(`.knight2Idle`);
+    document.querySelector(`.spriteContainer`).removeChild(idle);
+    $(`.spriteContainer`).append(`<div class="knight2Attack"></div>`);
+    
+    
+    setTimeout(setIdleKnightTwo, 1000);
+}
+
+function setIdleKnightTwo(){                                                //Knight Two Set Idle
+    let attack = document.querySelector(`.knight2Attack`);
+    document.querySelector(`.spriteContainer`).removeChild(attack);
+    
+    $(`.spriteContainer`).append(`<div class="knight2Idle"></div>`)
+}
+/*======== End ========*/
+
+//======== Win/Lose ========
+function winFight(){
+    document.body.classList.add(`game-overWin`);
+}
+function loseFight(){
+    
+}
+/*======== End ========*/
+
 attackButton();
 
-
-
+function testGen(){
+    let enemyInt = Math.floor(Math.random() * 30);
+    return enemyInt;
+    console.log(enemyInt);
+}
