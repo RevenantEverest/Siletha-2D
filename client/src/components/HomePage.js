c87fea3b58ba470801f5b7984b7fdb50de3afbc6
stefischer <stefischer@mail.com>
2018-03-28 16:08:12 -0400

    Home Page links to Story route

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,19 +1,56 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect } from 'react-router-dom';
+import HomePageCSS from '../public/style/HomePage.css'
+
+//Image Imports
+import Logo from '../public/images/Logo.png';
+
+//Audio Imports
+import HomePageTheme from '../public/sounds/FantasyMusic-TheLastOfHisName.wav';
+import HomePagePlayButtonSound from '../public/sounds/buttonPress';
 
 class HomePage extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      fireRedirect: false
+    }
+  }
+
+  componentDidMount() {
+    this.playHomePageTheme();
+  }
+
+  playHomePageTheme() {
+    let HomePageTheme = document.querySelector('.HomePage-audio-main');
+    HomePageTheme.play();
+  }
+
+  handlePlayButton() {
+    let HomePagePlayButtonSound = document.querySelector('.HomePage-audio-playButtonClick');
+    HomePagePlayButtonSound.play();
+    setTimeout(() => {
+      this.setState({
+        fireRedirect: true
+      })
+    },1000)
+  }
 
   render() {
     return(
       <div className="HomePage">
-        <h1>Home Page</h1>
-        <img src="images/Logo.png" />
-        <nav>
-            <button className="playButton">Play</button>
-        </nav>
+        <img className="HomePage-Logo" src={Logo}></img>
+        <button className="HomePage-playButton" onClick={(e) => this.handlePlayButton()}>Play</button>
         <div className="HomePage-audio">
-          <audio src="Sounds/Fantasy%20Music%20-%20The%20Last%20of%20His%20Name.wav"></audio>
-          <audio className="play" src="Sounds/Effects/UI/buttonPress"></audio>
+          <audio className="HomePage-audio-main" src={HomePageTheme}></audio>
+          <audio className="HomePage-audio-playButtonClick" src={HomePagePlayButtonSound}></audio>
         </div>
+        {this.state.fireRedirect ? <Redirect to="/Story" /> : ''}
       </div>
     );
   }
--- client/src/components/Story.js
+++ client/src/components/Story.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 
+import StoryCSS from '../public/style/Story.css';
+
 class Story extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      narritive: `The world of Acirhia was not always the peaceful place we all know. The Realm of Siletha is known for the peace of it's people.`
+    }
+  }
 
   render() {
     return(
       <div className="Story">
-        <h1>Story Page</h1>
-        <div className="">
-          <h1 className="one">The world of Acirhia was not always the peaceful place we all know. The Realm of Siletha is known for the peace of it's people. </h1>
+        <div className="Story-Contents-Container">
+          <h1 className="Story-Contents">{this.state.narritive}</h1>
         </div>
         <div className="Story-audio">
           <button type="button" className="nextButton">NEXT</button>
